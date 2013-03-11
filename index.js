1ea6982fa1a59ef3e427e24d9fd997724adf44d3
Christian Wesselhoeft <christian@cwesselhoeft.com>
2013-03-11 10:48:56 -0700

    Move modifiers into JSON file.

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
-var modifiers = {
+var modifiers = require('./modifiers')
 
-  // Slot modifiers
-  slot:
-  { head: 1
-  , neck: 0.5625
-  , shoulder: 0.75
-  , shirt: 0
-  , chest: 1
-  , waist: 0.75
-  , legs: 1
-  , feet: 0.75
-  , wrist: 0.5625
-  , hands: 0.75
-  , finger1: 0.5625
-  , finger2: 0.5625
-  , trinket1: 0.5625
-  , trinket2: 0.5625
-  , back: 0.5625
-  , mainHand: 2
-  , offHand: 1
-  , ranged: 0.3164
-  , tabard: 0
-  }
-
-  // Hunter weapon slot modifiers
-, slotHunter:
-  { mainHand: 0.3164
-  , offHand: 0.3164
-  , ranged: 5.3224
-  }
-
-  // Quality modifiers
-, qualities:
-  [ [73, 1, 0.005]
-  , [73, 1, 0.005]
-  , [73, 1, 1]
-  , [81.375, 0.8125, 1]
-  , [91.45, 0.65, 1]
-  , [91.45, 0.65, 1.3]
-  , null
-  , [81.375, 0.8125, 1]
-  ]
-
-  // Quality modifiers for ilvls below 120
-, qualitiesLow:
-  [ [8, 2, 0.005]
-  , [8, 2, 0.005]
-  , [8, 2, 1]
-  , [0.75, 1.8, 1]
-  ]
-
-  // Convenience method for retrieving quality
-, quality: function(ilvl, quality) {
-    return ilvl > 120 || quality > 3 ?
-      this.qualities[quality] : this.qualitiesLow[quality]
+// Convenience method for retrieving quality
+var quality = function(ilvl, quality) {
+  if (ilvl > 120 || quality > 3) {
+    return modifiers.qualities[quality]
+  } else {
+    return modifiers.qualitiesLow[quality]
   }
 }
-
 
 module.exports = function(character) {
   var score = 0
@@ -69,7 +20,7 @@ module.exports = function(character) {
     if (!items.hasOwnProperty(slot)) { continue }
 
     var item = items[slot]
-      , qualityMod = modifiers.quality(item.itemLevel, item.quality)
+      , qualityMod = quality(item.itemLevel, item.quality)
       , slotMod = modifiers.slot[slot]
 
     if (slot === 'mainHand' && items.offHand !== undefined) {
